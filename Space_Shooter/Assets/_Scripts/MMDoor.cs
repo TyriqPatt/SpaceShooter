aad1c8332bdcf212cbf67f82bf9d214d6c783f6a
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2020-04-23 01:28:18 -0400

    Added doors to minimap ui

---
+++ Space_Shooter/Assets/_Scripts/MMDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MMDoor : MonoBehaviour
+{
+
+    public Image MiniMapUI;
+    public GameObject Wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MiniMapUI = GetComponent<Image>();
+        MiniMapUI.color = Color.yellow;
+        //MiniMapUI.color = Color.green;
+        //MiniMapUI.color = Color.red;
+        if(Wall.activeSelf == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MiniMapUI.color = Color.green;
+        }
+    }
+}
