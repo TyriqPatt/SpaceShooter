438263dcb2145e1469213e19cf04dede4b5635d0
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-04-25 22:38:02 -0400

    Enemy now share pool and can shoot different bullets now

---
+++ Space_Shooter/Assets/_Scripts/Jesse'sScripts/JR_EneObjectPooler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JR_EneObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size; 
+    }
+    #region Singleton
+    public static JR_EneObjectPooler Instance;
+    private void Awake()
+    {
+        Instance = this; 
+    }
+
+    #endregion
+     
+    public List<Pool> listOfPools; 
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    public GameObject BulletContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();  
+
+        foreach (Pool pool in listOfPools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>(); 
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+                obj.transform.parent = BulletContainer.transform;
+
+            }
+
+            poolDictionary.Add(pool.tag, objectPool); 
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+
+        if(!poolDictionary.ContainsKey(tag))
+        {
+            Debug.Log(" Pool with tag " + tag + " doesn't exist. ");
+            return null;  
+        }
+
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn; 
+    }
+
+   
+}
--- Space_Shooter/Assets/_Scripts/Jesse'sScripts/JR_SingleShotStyle.cs
+++ Space_Shooter/Assets/_Scripts/Jesse'sScripts/JR_SingleShotStyle.cs
@@ -9,7 +9,17 @@ public class JR_SingleShotStyle : MonoBehaviour
 
     public float Firerate = 4f;
     public float nextTimeToFire = 0f;
+    //JR_EnemyObjectPooling m_objectPooling;
+    public JR_EneObjectPooler objectPooler; 
 
+    public enum State
+    {
+        Fire,
+        Lighting,
+        BlackHole,
+        Double
+    }
+    public State _state; 
     
 
     //JR_EnemyObjectPooling m_enemyOjectPooling; 
@@ -18,7 +28,8 @@ public class JR_SingleShotStyle : MonoBehaviour
     void Start()
     {
         //barrel = gameObject.transform.GetChild(0).transform;
-
+        // m_objectPooling = GetComponent<JR_EnemyObjectPooling>(); 
+        objectPooler = JR_EneObjectPooler.Instance; 
     }
 
     // Update is called once per frame
@@ -38,18 +49,34 @@ public class JR_SingleShotStyle : MonoBehaviour
 
     void SingleFire()
     {
-        for (int i = 0; i < JR_EnemyObjectPooling.Instance.ObjectList.Count; i++)
+        switch(_state)
         {
-            if (JR_EnemyObjectPooling.Instance.ObjectList[i].activeInHierarchy == false)
-            {
-                JR_EnemyObjectPooling.Instance.ObjectList[i].SetActive(true);
-                JR_EnemyObjectPooling.Instance.ObjectList[i].transform.position = barrel.transform.position;
-                JR_EnemyObjectPooling.Instance.ObjectList[i].transform.rotation = barrel.transform.rotation;
-
-                break;
-            }
+            case State.Fire:
+                {
+                    objectPooler.SpawnFromPool("Fire", barrel.transform.position, barrel.transform.rotation);
+                    nextTimeToFire = 0;
+                    break; 
+                }
+            case State.Lighting:
+                {
+                    objectPooler.SpawnFromPool("Lighting", barrel.transform.position, barrel.transform.rotation);
+                    nextTimeToFire = 0;
+                    break;
+                }
+            case State.BlackHole:
+                {
+                    objectPooler.SpawnFromPool("BlackHole", barrel.transform.position, barrel.transform.rotation);
+                    nextTimeToFire = 0;
+                    break;
+                }
+            case State.Double:
+                {
+                    objectPooler.SpawnFromPool("Double", barrel.transform.position, barrel.transform.rotation);
+                    nextTimeToFire = 0;
+                    break;
+                }
         }
-        nextTimeToFire = 0;
+        
 
       
     }
