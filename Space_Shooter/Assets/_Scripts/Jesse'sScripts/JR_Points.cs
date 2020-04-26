da6b30e0e38c9f01a22bcc803b11e8df6f81b588
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-04-26 02:14:10 -0400

    Added point script

---
+++ Space_Shooter/Assets/_Scripts/Jesse'sScripts/JR_Points.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JR_Points : MonoBehaviour
+{
+    public bool isTaken = false;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy" )
+        {
+
+        }
+    }
+
+
+}
