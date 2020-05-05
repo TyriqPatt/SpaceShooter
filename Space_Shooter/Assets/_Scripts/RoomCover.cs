49f475167b3a505c4369c418b4840cc883e9dd55
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2020-05-05 01:29:23 -0400

    added random cover for each room

--- Space_Shooter/Assets/_Scripts/RoomCover.cs
+++ Space_Shooter/Assets/_Scripts/RoomCover.cs
@@ -6,11 +6,21 @@ public class RoomCover : MonoBehaviour
 {
 
     public GameObject[] Cover;
-
+    public int rand;
+    public int scasca;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scasca = Cover.Length;
+        rand = Random.Range(0, 10);
+        if(rand < Cover.Length - 1)
+        {
+            Instantiate(Cover[rand], transform.position, transform.rotation);
+        }
+        else if (rand >= Cover.Length)
+        {
+            
+        } 
     }
 
     // Update is called once per frame
