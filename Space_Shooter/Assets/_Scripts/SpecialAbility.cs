e9044e31c447a59097ff770c4b4a84a12a2eb7da
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2020-08-30 00:19:55 -0400

    updated scout special ability(not finished)

--- Space_Shooter/Assets/_Scripts/SpecialAbility.cs
+++ Space_Shooter/Assets/_Scripts/SpecialAbility.cs
@@ -9,6 +9,8 @@ public class SpecialAbility : MonoBehaviour
     public float TankShotCooldown;
     public GameObject ScoutHolo;
     Canons canons;
+    public GameObject Length;
+    bool ability;
     public enum State { Tank, Commander, Scout }
 
     public State ClassState;
@@ -41,36 +43,61 @@ public class SpecialAbility : MonoBehaviour
         
         if (ClassState == State.Scout)
         {
+           
+
             if (ScoutTeleportCooldown > 0)
             {
                 ScoutTeleportCooldown -= Time.deltaTime;
             }
             else
             {
-                RaycastHit hit;
-                if (Physics.SphereCast(ScoutHolo.transform.position, 5, transform.position, out hit))
+                if (Input.GetButton("Fire2"))
                 {
-                    
+                    Length.SetActive(true);
                 }
-
                 if (Input.GetButtonUp("Fire2"))
                 {
                     ScoutHolo.SetActive(true);
                     ScoutHolo.transform.parent = null;
                     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                     RaycastHit Hit;
-
+                    Length.SetActive(false);
+                    ability = true;
                     if (Physics.Raycast(ray, out Hit))
                     {
-                        if(hit.transform.tag != "Obstruction" || hit.transform.name != "Wall")
+                        if(Hit.transform.tag != "Obstruction" && Hit.transform.name != "Wall")
                         {
-                            transform.parent.position = new Vector3(Hit.point.x, 2.5f, Hit.point.z);
+                            //transform.parent.position = new Vector3(Hit.point.x, 2.5f, Hit.point.z);
                             Debug.Log("cuyhagdhga");
                         }
-                        
                     }
                     ScoutTeleportCooldown = 5;
                 }
+            }
+            if (ability)
+            {
+                transform.parent.position += transform.forward * 5;
+                RaycastHit hit;
+                // Does the ray intersect any objects excluding the player layer
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+                {
+                    if (hit.transform.name == "Wall" || hit.transform.tag == "Obstruction")
+                    {
+                        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    }
+                }
+                if (hit.distance <= 4 && hit.transform.name == "Wall" || hit.distance <= 4 && hit.transform.tag == "Obstruction")
+                {
+                    ability = false;
+                }
+                if (ScoutTeleportCooldown < 4.9f)
+                {
+                    //transform.parent.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    ability = false;
+                    
+                }
+                //transform.parent.position = Vector3.MoveTowards(transform.parent.position, Hit.transform.position, 5 * Time.deltaTime);
+
             }
         }
     }
