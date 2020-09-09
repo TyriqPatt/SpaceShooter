037405b6fb9ae239e38c90446fcd844e4b63c139
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2020-09-08 20:16:32 -0400

    Added melee enemy

--- Space_Shooter/Assets/_Scripts/Orbit.cs
+++ Space_Shooter/Assets/_Scripts/Orbit.cs
@@ -7,22 +7,35 @@ public class Orbit : MonoBehaviour
 
     public Transform Target;
     public float speed;
+    public float TimeTilSwitch;
+    public bool pingpong;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (pingpong)
+        {
+            StartCoroutine(reverse(TimeTilSwitch));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Target.position, Vector3.down, speed);
+        if (pingpong)
+        {
+            transform.RotateAround(Target.position, Vector3.down, speed);
+        }
+        else
+        {
+            transform.RotateAround(Target.position, Vector3.down, speed);
+        }
+    }
 
-
-
-
-
-
+    IEnumerator reverse(float time)
+    {
+        yield return new WaitForSeconds(time);
+        speed = -speed;
+        StartCoroutine(reverse(TimeTilSwitch + .3f));
     }
 }
