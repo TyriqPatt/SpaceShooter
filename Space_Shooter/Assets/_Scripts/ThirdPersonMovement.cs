bb2bef7def318d59c516726464e971776b570a22
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2020-09-24 16:48:25 -0400

    u

--- Space_Shooter/Assets/_Scripts/ThirdPersonMovement.cs
+++ Space_Shooter/Assets/_Scripts/ThirdPersonMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +6,16 @@ public class ThirdPersonMovement : MonoBehaviour
 {
 
     CharacterController controller;
+    public Animator Anim;
     public float moveSpeed = 2;
     float curSpeed;
     float speedSmoothVel;
     float speedSmoothTime = .1f;
     float rotSpeed = .1f;
     float gravity = 3;
+    int comboChain;
+    float ComboReset;
+
 
     Transform MainCam;
 
@@ -26,12 +30,13 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         Move();
+        attack();
     }
 
     private void Move()
     {
         Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector3 forward = MainCam.forward;
+        Vector3 forward = MainCam.up;
         Vector3 right = MainCam.right;
 
         forward.y = 0;
@@ -48,9 +53,19 @@ public class ThirdPersonMovement : MonoBehaviour
             gravityVector.y -= gravity;
         }
 
-        if(desiredMoveDir != Vector3.zero)
+        if (Anim.GetCurrentAnimatorStateInfo(0).IsName("BasicSlash") || Anim.GetCurrentAnimatorStateInfo(0).IsName("BasicSlash2") || Anim.GetCurrentAnimatorStateInfo(0).IsName("BasicSlash3"))
+        {
+            desiredMoveDir = Vector3.zero;
+        }
+
+        if (desiredMoveDir != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDir), rotSpeed);
+            Anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            Anim.SetBool("isWalking", false);
         }
 
         float targetSpeed = moveSpeed * movementInput.magnitude;
@@ -58,5 +73,44 @@ public class ThirdPersonMovement : MonoBehaviour
 
         controller.Move(desiredMoveDir * curSpeed * Time.deltaTime);
         controller.Move(gravityVector * Time.deltaTime);
+    }
+
+    void attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(comboChain == 0)
+            {
+                comboChain += 1;
+                ComboReset = 0;
+                Anim.SetBool("IsinCombo", true);
+                Anim.SetTrigger("Attack");
+               
+            }
+            if (Anim.GetCurrentAnimatorStateInfo(0).IsName("BasicSlash"))
+            {
+                comboChain += 1;
+                ComboReset = 0;
+                Anim.SetBool("IsinCombo", true);
+                Anim.SetTrigger("Attack2");
+            }
+            if (Anim.GetCurrentAnimatorStateInfo(0).IsName("BasicSlash2"))
+            {
+                comboChain += 1;
+                ComboReset = 0;
+                Anim.SetBool("IsinCombo", true);
+                Anim.SetTrigger("Attack3");
+            }
+
+        }
+        if(comboChain > 0)
+        {
+            ComboReset += Time.deltaTime;
+            if(ComboReset >= 1f)
+            {
+                comboChain = 0;
+                Anim.SetBool("IsinCombo", false);
+            }
+        }
     }
 }
